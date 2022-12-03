aa078707feaa59262d73ef1939d26f79a90f0a16
jowax <8604164+jowax@users.noreply.github.com>
2022-12-03 10:27:04 +0000

    Day 1 task 2

--- 2022/day-one/src/lib.rs
+++ 2022/day-one/src/lib.rs
@@ -15,6 +15,26 @@ pub fn first_task(input: &str) -> i32 {
     *entries.first().unwrap()
 }
 
+pub fn second_task(input: &str) -> i32 {
+    let list = input.to_grouped_by_double_newline().to_grouped_int_vec();
+
+    let mut entries = list
+        .into_iter()
+        .map(|it| it.iter().sum::<i32>())
+        .collect::<Vec<_>>();
+
+    entries.sort();
+    entries.reverse();
+
+    let top_three = [
+        entries.first().unwrap(),
+        entries.get(1).unwrap(),
+        entries.get(2).unwrap(),
+    ];
+
+    top_three.into_iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,6 +46,22 @@ mod tests {
 
     #[test]
     fn first() {
-        println!("First result: {:#?}", first_task(include_str!("first_input.txt")));
+        println!(
+            "First result: {:#?}",
+            first_task(include_str!("first_input.txt"))
+        );
+    }
+
+    #[test]
+    fn test_second() {
+        assert_eq!(second_task(include_str!("first_test_input.txt")), 45000);
+    }
+
+    #[test]
+    fn second() {
+        println!(
+            "Second result: {:#?}",
+            second_task(include_str!("first_input.txt"))
+        );
     }
 }
